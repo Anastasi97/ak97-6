d5507560ac6459b9ce8e7855dfe59d85e9b52da1
Anastasi <vashpopo97@mail.ru>
2023-04-10 15:08:12 +0300

    Выполнение домашнего задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BmiService service = new BmiService();
+        double weightInKg = 98; // Вес
+        double heightInMeters = 1.87; // Рост в метрах
+        int bmi = (int) service.calculate(weightInKg, heightInMeters);
+        System.out.println(bmi);
     }
 }
