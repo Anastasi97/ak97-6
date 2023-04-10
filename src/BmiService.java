bd0769ee23c7d046e2c86573223993fc8943c1c9
Anastasi <vashpopo97@mail.ru>
2023-04-10 17:24:31 +0300

    Изменила названия

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate(double a, double b) {
-        double result = a / (b * b);
+    public double calculate(double weightInKg, double heightInMeters) {
+        double result = weightInKg / (heightInMeters * heightInMeters);
         return (int) result;
     }
 }
